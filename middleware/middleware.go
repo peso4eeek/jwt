276f43e3f5886ba7c4561d04e75904c9892e3d4d
peso4eek <grizimindmit@mail.ru>
2025-05-21 09:07:14 +0300

    fix middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -11,32 +11,34 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func JWTMiddleWare(next echo.HandlerFunc, secret string, appId int) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		tokenString := c.Request().Header.Get("Authorization")
-		if tokenString == "" {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Missing token"})
-		}
-		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-		claims := &core.JwtClaims{}
-
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+func JwtParser(secret string, appId int) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			tokenString := c.Request().Header.Get("Authorization")
+			if tokenString == "" {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Missing token"})
 			}
-			return []byte(secret), nil
-		})
-		if err != nil || !token.Valid {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid token"})
-		}
-		if claims.App_id != appId {
-			return c.JSON(http.StatusUnauthorized, map[string]string{
-				"Message": "invalid app id",
+			tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+			claims := &core.JwtClaims{}
+
+			token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+				if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+				}
+				return []byte(secret), nil
 			})
+			if err != nil || !token.Valid {
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid token"})
+			}
+			if claims.App_id != appId {
+				return c.JSON(http.StatusUnauthorized, map[string]string{
+					"Message": "invalid app id",
+				})
+			}
+			c.Set("uid", claims.Uid)
+			c.Set("email", claims.Email)
+			c.Set("app_id", claims.App_id)
+			return next(c)
 		}
-		c.Set("uid", claims.Uid)
-		c.Set("email", claims.Email)
-		c.Set("app_id", claims.App_id)
-		return next(c)
 	}
 }
